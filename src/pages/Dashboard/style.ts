f8c7af63ca0c4629a488af83f3f0d7ba6d78ae01
eduardoRduraes <eduardoduras.bsi@gmail.com>
2020-11-23 23:30:02 -0300

    projeto concluido

--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -1,5 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 65px;
@@ -11,7 +16,7 @@ export const Title = styled.h1`
 
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -25,88 +30,92 @@ export const Form = styled.form`
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
     font-weight: bold;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${(props) => props.hasError && css`border-color: #c53030;`}
 
     &::placeholder{
       color: #a8a8b3;
     }
   }
 
-  button{
-    width: 210px;
-    height: 70px;
-    background: #04D361;
-    border-radius: 0px 5px 5px 0px;
-    border: 0;
-    color: #fff;
-    font-weight: bold;
-    font-size:18px;
-    transition: background-color 0.2s;
+button{
+  width: 210px;
+  height: 70px;
+  background: #04D361;
+  border-radius: 0px 5px 5px 0px;
+  border: 0;
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+  transition: background - color 0.2s;
 
     &:hover{
-      background: ${shade(0.2, '#04d361')};
-    }
+    background: ${shade(0.2, '#04d361')};
   }
+}
 `;
 
 export const Error = styled.span`
-  display:block;
-  color: #c53030;
-  margin-top: 8px;
+display: block;
+color: #c53030;
+margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
-  margin-top: 80px;
-  max-width: 700px;
+margin-top: 80px;
+max-width: 700px;
 
-  a{
-    background: #fff;
-    border-radius: 5px;
-    width: 100%;
-    padding: 24px;
-    display:block;
-    text-decoration: none;
+a{
+  background: #fff;
+  border-radius: 5px;
+  width: 100%;
+  padding: 24px;
+  display: block;
+  text-decoration: none;
 
-    display: flex;
-    align-items: center;
-    
-    transition: transform 0.2s;
+  display: flex;
+  align-items: center;
+
+  transition: transform 0.2s;
 
    
 
     &:hover{
-      transform: translateX(10px);
-    }
-
-    &+a{
-      margin-top: 16px;
-    }    
-
-    img{
-      width: 64px;
-      height: 64px;
-      border-radius: 50%;
-      
-    }
-    div{
-      margin-left: 18px;
-      flex: 1;
-    }
-
-    strong{
-      font-size: 20px;
-      font-weight: bold;
-      color: #3d3d4d
-    }
-    p{
-      font-size: 18px;
-      color: #a8a8b3;
-      margin-top: 4px;
-    }
-
-    svg{
-      margin-left:auto;
-      color: #3d3d4d;
-    }    
+    transform: translateX(10px);
   }
+
+    & +a{
+    margin-top: 16px;
+  }
+
+  img{
+    width: 64px;
+    height: 64px;
+    border-radius: 50 %;
+
+  }
+  div{
+    margin-left: 18px;
+    flex: 1;
+  }
+
+  strong{
+    font-size: 20px;
+    font-weight: bold;
+    color: #3d3d4d
+  }
+  p{
+    font-size: 18px;
+    color: #a8a8b3;
+    margin-top: 4px;
+  }
+
+  svg{
+    margin-left: auto;
+    color: #3d3d4d;
+  }
+}
 `;
 
